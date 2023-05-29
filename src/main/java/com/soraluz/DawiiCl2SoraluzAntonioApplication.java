1e30057c2a32eb8a793c284a262a0675af007804
Antonio Soraluz <soraluztorresjoseantonio@gmail.com>
2023-05-29 15:57:32 -0500

    Se creó los html y la estructura del proyecto

---
+++ src/main/java/com/soraluz/DawiiCl2SoraluzAntonioApplication.java
@@ -0,0 +1,13 @@
+package com.soraluz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl2SoraluzAntonioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl2SoraluzAntonioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soraluz/DawiiCl2SoraluzAntonioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soraluz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl2SoraluzAntonioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
